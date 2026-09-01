4732e7071bcf5a73e901316360e29e2e97cb4f33
olivato <olivatooo@gmail.com>
2026-09-01 16:29:32 +0100

    Cover nested types and inherited serialisation in the reorder fixture

--- Tests~/MemberReorder/fixture/Fixture.cs
+++ Tests~/MemberReorder/fixture/Fixture.cs
@@ -1,3 +1,23 @@
+// Copyright 2025 Code Philosophy
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 using System;
 using System.Runtime.InteropServices;
 using UnityEngine;
@@ -66,4 +86,41 @@ namespace Fx
     public class Plain08 { public int f1, f2, f3, f4, f5, f6; public void A(){} public void B(){} public void C(){} public void D(){} public void E(){} public void F(){} }
     public class Plain09 { public int Prop1 {get;set;} public int Prop2 {get;set;} public int Prop3 {get;set;} public int Prop4 {get;set;} }
     public class Plain10 { public event Action E1; public event Action E2; public event Action E3; public event Action E4; public void Fire(){E1();E2();E3();E4();} }
+}
+
+namespace Fx2
+{
+    using System.Collections;
+
+    public class Typical : UnityEngine.MonoBehaviour
+    {
+        Func<int, int> a = x => x + 1;
+        Func<int, int> b = x => x + 2;
+        public IEnumerator Co() { yield return null; }
+        public IEnumerator Co2() { yield return null; }
+    }
+
+    public class PlainHost
+    {
+        Func<int, int> a = x => x + 1;
+        Func<int, int> b = x => x + 2;
+        public IEnumerator Co() { yield return null; }
+        public IEnumerator Co2() { yield return null; }
+        public class NestedOne { public int v; }
+        public class NestedTwo { public int v; }
+        public enum NestedMode { Off, On, Auto }
+    }
+
+    public class PlainBase { public int a; public string b; public float c; public bool d; public int e; }
+
+    [Serializable]
+    public class SerChild : PlainBase { public int x; public int y; }
+
+    public class PlainGrand { public int g1; public string g2; public float g3; public bool g4; public int g5; }
+    public class PlainMid : PlainGrand { public int m1; public string m2; public float m3; }
+
+    [Serializable]
+    public class SerLeaf : PlainMid { public int leaf; }
+
+    public class Holder : UnityEngine.MonoBehaviour { public SerChild data; public SerLeaf deep; }
 }
